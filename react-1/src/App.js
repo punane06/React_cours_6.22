617885f4fb42e4e2205d47d4cd96ad9e2f3a3333
punane <kadikerner@gmail.com>
2022-06-07 11:20:29 +0300

    task1

--- react-1/src/App.js
+++ react-1/src/App.js
@@ -1,17 +1,14 @@
-import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Avaleht from "./pages/Avaleht";
 
 function App() {
   return (
-    <div className="App">
-      <Link to="/">
-        <button>Avalehele</button>
-      </Link>
-      <Routes>
-        {/* localhost:3000/     HTML */}
-        <Route path="" exact element={<Avaleht />} />
-      </Routes>
+    <div className="app">
+      <button>Helehall tekst</button>
+      <p>Sinine tekst, kus hover muudab teksti suurust</p>
+      <img
+        src="https://scontent-arn2-2.xx.fbcdn.net/v/t1.6435-9/95587410_10213438351628257_8057774455964827648_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=19026a&_nc_ohc=wpQuPvRPoxoAX_YVrS7&tn=nRcS7pTDbZ0BVnq9&_nc_ht=scontent-arn2-2.xx&oh=00_AT-KVkbe14bZnHYblEVEl4qqUNtKgna1thySWyTlVrLMiA&oe=62C49173"
+        alt="pilt"
+      />
     </div>
   );
 }
